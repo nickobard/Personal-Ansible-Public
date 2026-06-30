01d5f333c13a7a67724592d122fe092a62abad67
Nikita Bardatskii <bardanik@fit.cvut.cz>
2026-06-30 15:26:56 +0200

    Add first bootstrap client script

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Personal-Ansible-Public
+
+
+Run the according script:
+
+Fast less secure way:
+
+`bash <(curl -fsSL https://raw.githubusercontent.com/nickobard/Personal-Ansible-Public/main/<script-name>.sh)`
+
+More secure way (inspect the code):
+
+```shell
+curl -fsSL https://raw.githubusercontent.com/nickobard/Personal-Ansible-Public/main/<script-name>.sh | bash
+```
+
+Client set up:
+
+`curl -fsSL https://raw.githubusercontent.com/nickobard/Personal-Ansible-Public/main/bootstrap-client.sh | bash`
+
+Pi5 set up:
+
+`curl -fsSL https://raw.githubusercontent.com/nickobard/Personal-Ansible-Public/main/bootstrap-pi.sh | bash`
---
+++ bootstrap-client.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -euox pipefail
+
+
+# pipx is per application python environment. It allows distribution of end tools - not libraries, but tools, ready to use in CLI.
+sudo dnf install -y git python3-pip pipx
+
+pipx ensurepath
+
+# Install Ansible tools isolated from system Python
+pipx install --include-deps ansible
+pipx install --include-deps ansible-lint
+
+# Make tools available in this script immediately
+export PATH="$HOME/.local/bin:$PATH"
+
+ansible --version
+ansible-lint --version
+
+# Repository with ansible set up
+
+ANSIBLE_DIR="$HOME/.ansible"
+INFRA_DIR="$ANSIBLE_DIR/infra"
+
+mkdir -p "$ANSIBLE_DIR"
+
+if [ ! -d "$INFRA_DIR/.git" ]; then
+    git clone "https://github.com/nickobard/Personal-Ansible-Public.git" "$INFRA_DIR"
+else
+    git -C "$INFRA_DIR" pull --ff-only
+fi
+
+cd "$INFRA_DIR"
+
+# ansible-galaxy collection install -r requirements.yml
+# ansible-lint
+# ansible-playbook playbooks/client.yml
